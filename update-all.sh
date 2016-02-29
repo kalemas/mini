57dfaed263a4fcce21c9fb50705b000007bfaf7f
Konstantin Maslyuk <kostyamaslyuk@gmail.com>
2016-02-29 23:51:54 +0300

    Switched repository url

--- update-all.sh
+++ update-all.sh
@@ -1,19 +1,3 @@
 #!/bin/sh
 
-#git submodule foreach "(git checkout master; git pull)&"
-
-cd bibletime
-git checkout mini
-git pull
-
-cd ../sword
-git checkout master
-git pull
-
-cd ../clucene
-git checkout master
-git pull
-
-cd ../curl
-git checkout master
-git pull
+git submodule foreach "(git checkout master; git pull)&"
