8c2fa2047ddeec8c19a014fc1cb789b4a961a20e
Konstantin Maslyuk <kostyamaslyuk@gmail.com>
2016-03-01 00:05:13 +0300

    Update script now works

--- update-all.sh
+++ update-all.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-git submodule foreach "(git checkout master; git pull)&"
+git submodule init
+git submodule update
+git submodule foreach "(git checkout master; git pull)"
+
+read -p "press Enter to continue"
